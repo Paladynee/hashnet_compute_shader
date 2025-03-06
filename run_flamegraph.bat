cd2f8df049bf7050afd9391007926f7808035d75
Voxell Paladynee <paladyneshadow@gmail.com>
2025-03-07 02:48:55 +0300

    really remove run_flamegraph now lol

--- run_flamegraph.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "Start-Process -Verb RunAs powershell -ArgumentList '-Command "cd C:\Users\eurydice\Desktop\hashnet_rust\hashnet_compute_shader; cargo.exe +nightly flamegraph --release ;pause"'"
